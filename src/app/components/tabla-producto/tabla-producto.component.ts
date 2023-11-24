e4d94bb28f52b02929144dcd351b89f1729529b2
marlonds95 <Marlonds95@users.noreply.github.com>
2023-11-23 20:41:09 -0500

    mensajes eliminados

--- src/app/components/tabla-producto/tabla-producto.component.ts
+++ src/app/components/tabla-producto/tabla-producto.component.ts
@@ -22,22 +22,21 @@ export class TablaProductoComponent {
     this.carrito.push(producto);
     console.log('Producto agregado al carrito:', producto);
     alert("Se agrego Correctamente continua comprando.")
-    // Puedes realizar acciones adicionales aquí, como actualizar la interfaz de usuario
+   
   }
 
-  // Método para eliminar productos del carrito
+  
   removeFromCart(producto: any): void {
     this.carrito = this.carrito.filter(item => item !== producto);
     console.log('Producto eliminado del carrito:', producto);
-    // Puedes realizar acciones adicionales aquí, como actualizar la interfaz de usuario
+    
   }
 
-  // Método para guardar el carrito y navegar a la página de la tienda
+  
   guardarCarrito(): void {
     this.serviceprod.guardarCarritoEnTienda(this.carrito);
     console.log('Guardando carrito:', this.carrito);
-    // Puedes realizar acciones adicionales aquí, si es necesario
-    // Por ejemplo, podrías limpiar el carrito después de guardarlo
+    
     this.carrito = [];
     this.ruta.navigate(['tienda']);
   }
